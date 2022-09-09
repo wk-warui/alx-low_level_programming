f76be5ee64a7b3ef0867b784b8fac7656d2c1942
wk-warui <demarazeandrena@gmail.com>
2022-09-09 07:15:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char a = 'a';
+	int n = 0;	
+	while (n < 26)
+	{
+		putchar(a);
+		a++;
+		n++;
+	}
+	putchar('\n');
+	Return(0);
+}
