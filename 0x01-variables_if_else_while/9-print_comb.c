bf34c8deda076b3acf7c564a63eadb3a1124ac46
wk-warui <demarazeandrena@gmail.com>
2022-09-09 13:34:32 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
