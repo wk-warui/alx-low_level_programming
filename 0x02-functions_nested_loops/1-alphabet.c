b189d122fae512cdf161a281f57446f741ccc12a
wk-warui <demarazeandrena@gmail.com>
2022-09-13 08:30:28 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet - print_alphabets
+ *
+ * Relays explained stuff.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0(Success)
+ */
+
