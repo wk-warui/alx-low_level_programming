05f67ddd55681d70312dd69dd075a928cbcc3694
wk-warui <demarazeandrena@gmail.com>
2022-09-13 08:19:05 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H_   /* Include guard */
+#define MAIN_H_
+
+
+/* _putchar.c */
+void print_putchar(void);
+
+int _putchar(char c);
+
+/* 10 x alphabet */
+void print_alphabet(void);
+
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int n);
+
+int print_sign(int n);
+
+int _abs(int c);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+#endif 
