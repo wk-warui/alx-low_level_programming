0f0e716a75559e50a90ae5bb18e00731d9737a3e
wk-warui <demarazeandrena@gmail.com>
2022-09-15 08:01:23 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplication
+ * @a: tested integer
+ * @b: tested integer
+ *
+ * Return: Always 0(Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
