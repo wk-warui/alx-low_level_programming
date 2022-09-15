6d9d76743e3b624b9e75593076b89113a87ba34b
wk-warui <demarazeandrena@gmail.com>
2022-09-15 09:02:27 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - enter numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number < '9'; number++)
+	{
+		if ((number != 2) && (number != 4))
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
+}
