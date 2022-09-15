f96af3d73651a5a3316ef29c1e129d77a1098ebe
wk-warui <demarazeandrena@gmail.com>
2022-09-15 05:17:04 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-		largest = a;
+		printf("%d a");
 	}
 	else if (b > a && b > c)
 	{
-		largest = b;
+		printf("%d b");
 	}
 	else
 	{
-		largest = c;
+		printf("%d c");
 	}
 	return (largest);
 }
