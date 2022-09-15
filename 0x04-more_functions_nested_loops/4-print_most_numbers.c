3d2506b2772abbca25a9b744c66e039a07d416e1
wk-warui <demarazeandrena@gmail.com>
2022-09-15 09:10:36 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number = '0'; number < '9'; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
-		if (number != 2 && number != 4)
+		while (number != '2' && number != '4')
 		{
 			_putchar(number);
 		}
