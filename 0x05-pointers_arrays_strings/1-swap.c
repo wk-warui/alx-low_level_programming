ba2e6908c00e701b4cdaae888b3e77c9efa830ca
wk-warui <demarazeandrena@gmail.com>
2022-09-19 07:38:24 -0700

    swipe values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - interchanges the value of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = f;
+}
