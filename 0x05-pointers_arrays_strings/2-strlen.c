54cfa7b4aa85e025fd583722ff087fc014fb1382
wk-warui <demarazeandrena@gmail.com>
2022-09-19 07:57:01 -0700

    returns the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns lenght of a string
+ * @s: string
+ *
+ * Return: Always 0(Success)
+ */
+int _strlen(char *s)
+{
+	int q;
+
+	q = 0;
+	while (s[q] != '\0')
+	{
+		q++;
+	}
+	return (0);
+}
