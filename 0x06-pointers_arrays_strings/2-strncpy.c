2775b8f33dd9fb18840030c50dbd7f3d47cf9379
wk-warui <demarazeandrena@gmail.com>
2022-09-22 04:00:19 -0700

    copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int size, i;
 
-	size = 0;
-	while (dest[size] != '\0')
-	{
-		for (i = 0; i < n && src[i] != '\0'; i++, size++)
-			dest[i] = src[i];
-		for (; i < n; i++)
-			dest[i] = '\0';
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++, size++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
