b35036dbd78b90a8ce2278d3c5f31c0ca598149d
wk-warui <demarazeandrena@gmail.com>
2022-09-22 03:22:14 -0700

    concatebation of two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int length, l;
+	int length, j;
 
 	length = 0;
 	while (dest[length] != '\0')
 	{
 		length++;
 	}
-	for (l = 0; src[l] != '\0'; j++, length++)
+	for (j = 0; src[j] != '\0'; j++, length++)
 	{
-		dest[length] = src[l];
+		dest[length] = src[j];
 	}
 	dest[length] = '\0';
 	return (dest);
