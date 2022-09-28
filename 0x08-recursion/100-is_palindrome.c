cc4972f180675c35bea49222dad9532aedfdcfff
wk-warui <demarazeandrena@gmail.com>
2022-09-28 06:58:30 -0700

    checks if a string is empty

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks for an empty string
+ * @s: pointer to the string
+ *
+ * Return: 1 if string is empty, 0 if it is not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - recursively checks for an empty string
+ * @s: string to check
+ * @i: iterator
+ * @len: string length
+ *
+ * Return: 1 if string is empty, 0 if otherwise
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (i);
+	return (check_pal(s, i + 1, len - 1));
+}
