6469ecd86b7ca1f516d0dd4be77b753eb859cf9c
wk-warui <demarazeandrena@gmail.com>
2022-09-30 04:37:40 -0700

    a program that prints all arguments its receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,19 +7,15 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int count;
+	int count = 0;
 
-	printf("%s\n", argv[0]);
-
-	if (argc > 1)
+	while ( count < argc)
 	{
-		for (count = 0; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-		printf("\n");
+		printf("%s\n", *(argv + count));
+		count++;
+		(*argv++);
 	}
 
 	return (0);
