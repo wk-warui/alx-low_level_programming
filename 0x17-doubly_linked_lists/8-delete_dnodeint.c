046fa4d1b6df506c00a51064dbf22d2bb1793133
wk-warui <demarazeandrena@gmail.com>
2022-12-08 08:24:32 -0800

    a function that deletes the node at index index of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,18 +22,21 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (-1);
 		tmp = tmp->next;
 	}
+
 	if (tmp == *head)
 	{
 		*head = tmp->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
 	}
+
 	else
 	{
-		tmp->prev->next = tmp->prev;
+		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}
+
 	free(tmp);
 	return (1);
 }
