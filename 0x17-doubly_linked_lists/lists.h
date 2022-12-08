a397dcea0eec08ad06924635569245d47869d30d
wk-warui <demarazeandrena@gmail.com>
2022-12-08 05:27:43 -0800

    a function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,13 +1,8 @@
 #ifndef "LISTS_H"
 #define "LISTS_H"
 
-/**
- * file: lists.h
- * Auth: Kelvin Warui
- */
-
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include<string.h>
 
 /**
@@ -19,11 +14,12 @@
  * Description: doubly linked list node structure
  *
  */
+
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -36,4 +32,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+#endif
